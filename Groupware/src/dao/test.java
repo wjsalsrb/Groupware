96caef002f9bc791e9cf687d881f267644466698
goyu8744 <goyu8744@naver.com>
2017-08-31 16:44:31 +0900

    didididi

--- Groupware/src/dao/test.java
+++ Groupware/src/dao/test.java
@@ -3,6 +3,8 @@ package dao;
 public class test {
 	public static void main(String[] args){
 		System.out.println("抛胶飘沁促备asdfafasdfasdfasdfasdfasdf");
+		System.out.println("抛胶备asdfafasdfasdfasdfasdfasdf");
 		System.out.println("dsdsdsdsdsdsdsddsd具具具具具");
+		System.out.println("aaaaaaaaaaaaaaaaa");
 	}
 }
