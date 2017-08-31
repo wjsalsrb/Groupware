5a23c0abed111b5d5beb9f02ae732bc2a1f9de1b
KITRI <KITRI@KITRIUSER-PC>
2017-08-31 16:48:34 +0900

    iiii

--- Groupware/src/dao/test.java
+++ Groupware/src/dao/test.java
@@ -5,6 +5,6 @@ public class test {
 		System.out.println("抛胶飘沁促备asdfafasdfasdfasdfasdfasdf");
 		System.out.println("抛胶备asdfafasdfasdfasdfasdfasdf");
 		System.out.println("dsdsdsdsdsdsdsddsd具具具具具");
-		System.out.println("aaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbb");
 	}
 }
