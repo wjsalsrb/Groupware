e354e4ae20c9fa0ce4efaf0d87bc256cf48107b9
KITRI <KITRI@KITRIUSER-PC>
2017-08-31 16:31:50 +0900

    rftrt

--- Groupware/src/dao/test.java
+++ Groupware/src/dao/test.java
@@ -2,6 +2,6 @@ package dao;
 
 public class test {
 	public static void main(String[] args){
-		System.out.println("테스트했다구asdfasdfasdf");
+		System.out.println("테스트했다구asdfafasdfasdfasdfasdf");
 	}
 }
