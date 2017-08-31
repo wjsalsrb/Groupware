c3576d375ac52c2cc5d4b0afbb16ff18e55d2a16
goyu8744 <goyu8744@naver.com>
2017-08-31 16:42:12 +0900

    dsdsdsd

--- Groupware/src/dao/test.java
+++ Groupware/src/dao/test.java
@@ -3,6 +3,6 @@ package dao;
 public class test {
 	public static void main(String[] args){
 		System.out.println("테스트했다구asdfafasdfasdfasdfasdfasdf");
-		System.out.println("dsdsdsdsd");
+		System.out.println("dsdsdsdsdsdsdsddsd");
 	}
 }
